9d7f545e6168e2ecc5682edc2aa4332f7a6a1aac
Timo <timo.schessl@gmx.de>
2020-05-06 19:42:44 +0200

    POST works!

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import random
 
 import mysql
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from mysql import connector
 
 server = Flask(__name__)
@@ -24,6 +24,75 @@ def getDataById(urlId):
     Id = urlId
     cursor.execute("select * from user where id='"+Id+"'")
     return jsonify(cursor.fetchall())
+
+@server.route("/post",methods=["GET","POST"])
+def postData():
+
+    json = request.json
+    username = json["username"]
+    password = json["password"]
+    repeatedPassword = json["repeat"]
+
+
+    return insertNewRowIntoUserTable(username,password,repeatedPassword,request)
+
+
+def insertNewRowIntoUserTable(username, password, repeatedPassword, request):
+
+    if(len(username)==0):
+        return jsonify("Please enter a username!")
+
+    if (len(password) < 6):
+
+        return jsonify("Please enter a password (at least 6 characters)!")
+
+    if(password != repeatedPassword):
+
+        return jsonify("The passwords are not the same!")
+
+    if(checkIfUsernameExists(username)):
+
+        return jsonify("This username already exists!")
+
+    id = random.randint(0, 10000000)
+
+    while (checkIfIdExists(id)):
+        id = random.randint(0, 10000000)
+
+    cursor.execute(
+        "insert into user (id,username,password) VALUES ('" + str(id) + "','" + username + "','" + password + "')")
+    database.commit()
+    return request.json
+
+
+
+def checkIfUsernameExists(username):
+    usernameColumn = getColumnOfUserTable("username")
+
+    for element in usernameColumn:
+        for entry in element:
+            if (entry == username):
+                return True
+
+    return False
+
+def getColumnOfUserTable(columnName):
+    cursor.execute("select " + columnName + " from user")
+    result = cursor.fetchall()
+
+    return result
+
+
+def checkIfIdExists(id):
+    idColumn = getColumnOfUserTable("id")
+
+    for element in idColumn:
+        for entry in element:
+         if(entry==id):
+            return True
+
+    return False
+
 
 
 if __name__ == "__main__":
