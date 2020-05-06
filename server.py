8df18f087a5ac12667359086b26df8e82de5179d
Timo <timo.schessl@gmx.de>
2020-05-06 19:17:37 +0200

    GET request works! Added a start page

--- server.py
+++ server.py
@@ -1,9 +1,29 @@
-from flask import Flask
+import random
+
+import mysql
+from flask import Flask, jsonify
+from mysql import connector
+
 server = Flask(__name__)
+
+database = mysql.connector.connect(host="localhost", user="root", passwd="", database="besucher")
+
+cursor = database.cursor()
 
 @server.route("/")
 def startPage():
-    return "Hello World"
+    return "This is a REST API for a MySQL database"
+
+@server.route("/get")
+def getData():
+    cursor.execute("select * from user")
+    return jsonify(cursor.fetchall())
+
+@server.route("/get/<urlId>")
+def getDataById(urlId):
+    Id = urlId
+    cursor.execute("select * from user where id='"+Id+"'")
+    return jsonify(cursor.fetchall())
 
 
 if __name__ == "__main__":
